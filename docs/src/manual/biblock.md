4755030aa2def05c940cde5f1e0f99548d1919bc
mmider <marcin.mider@gmail.com>
2020-06-22 17:07:07 +0200

    additional functions for BiBlock

--- docs/src/manual/biblock.md
+++ docs/src/manual/biblock.md
@@ -7,18 +7,43 @@ DiffusionMCMCTools.BiBlock
 
 There are many functions implemented for it.
 
+#### Imputation of paths
+----
+```@docs
+DiffusionMCMCTools.draw_proposal_path!(bb::DiffusionMCMCTools.BiBlock)
+```
 
-#### For adjustments made after the accept-reject decision (regardless of what it was)
+#### Accept/reject decision in an MCMC setting
+-----
+```@docs
+DiffusionMCMCTools.accept_reject_proposal_path!(bb::DiffusionMCMCTools.BiBlock, mcmciter)
+```
+
+#### Adjustments made after the accept-reject decision (regardless of what it was)
 ----
 ```@docs
 DiffusionMCMCTools.set_accepted!(bb::DiffusionMCMCTools.BiBlock, i::Int, v)
 ```
 
-#### For adjustments to the containers in case of **acceptance** of proposals:
+#### Adjustments to the containers in case of **acceptance** of proposals:
 ----
 ```@docs
 DiffusionMCMCTools.swap_paths!(bb::DiffusionMCMCTools.BiBlock)
 DiffusionMCMCTools.swap_XX!(bb::DiffusionMCMCTools.BiBlock)
 DiffusionMCMCTools.swap_WW!(bb::DiffusionMCMCTools.BiBlock)
 DiffusionMCMCTools.swap_PP!(bb::DiffusionMCMCTools.BiBlock)
+```
+
+#### Setting up a block
+----
+```@docs
+DiffusionMCMCTools.set_obs!(bb::DiffusionMCMCTools.BiBlock)
+DiffusionMCMCTools.recompute_guiding_term!(bb::DiffusionMCMCTools.BiBlock)
+DiffusionMCMCTools.find_W_for_X!(bb::DiffusionMCMCTools.BiBlock)
+```
+
+#### Utility
+----
+```@docs
+DiffusionMCMCTools.ll_of_accepted(bb::DiffusionMCMCTools.BiBlock, i)
 ```
--- src/biblock.jl
+++ src/biblock.jl
@@ -55,6 +55,42 @@ mutable struct BiBlock{L,TGP,TGPl,TW,TWn,TX}
 end
 
 """
+    draw_proposal_path!(bb::BiBlock)
+
+Sample a proposal path, compute log-likelihood along the way. Assumes
+`bb.b.XX[1].x[1]` is a starting point. Uses preconditioned Crank-Nicolson scheme
+with memory parameter set as `bb.ρ`.
+"""
+function draw_proposal_path!(bb::BiBlock) end
+
+function draw_proposal_path!(bb::BiBlock{false})
+    success, bb.b°.ll = _draw_proposal_path!(bb)
+    success || return false # anything?
+    success, ll°_last = _draw_proposal_path_last_segment!(bb, y1)
+    bb.b°.ll += ll°_last
+    return success
+end
+
+function draw_proposal_path!(bb::BiBlock{true})
+    success, bb.b°.ll = _draw_proposal_path!(bb)
+    return success
+end
+
+function _draw_proposal_path!(bb::BiBlock)
+    rand!(
+        bb.b.PP, bb.b°.XX, bb.b°.WW, bb.b.WW, bb.ρ, _LL, bb.b.XX[1].x[1];
+        Wnr=bb.b°.Wnr
+    )
+end
+
+function _draw_proposal_path_last_segment!(bb::BiBlock, y1)
+    success, ll° = rand!(
+        bb.b.P_last[1], bb.b°.XX[end], bb.b°.WW[end], bb.b.WW[end], bb.ρ, _LL,
+        y1; Wnr=bb.b°.Wnr
+    )
+end
+
+"""
     set_accepted!(bb::BiBlock, i::Int, v)
 
 Commit the accept/reject decision `v` to acceptance history of BiBlock `b` at
@@ -109,3 +145,60 @@ function swap_PP!(bb::BiBlock)
     # on a terminal block i.e. BiBlock{true} this simply makes no difference
     bb.b.P_last[1], bb.b°.P_last[1] = bb.b°.P_last[1], bb.b.P_last[1]
 end
+
+"""
+    accept_reject_proposal_path!(bb::BiBlock, mcmciter)
+
+Accept/reject decision of the Metropolis-Hastings algorithm for the step of path
+imputation.
+"""
+function accept_reject_proposal_path!(bb::BiBlock, mcmciter)
+    accepted = rand(Exponential(1.0)) > -(bb.b°.ll - bb.b.ll)
+    accepted && swap_paths!(bb)
+    set_accepted!(bb, mcmciter, accepted)
+    save_ll!(bb.b, mcmciter)
+    save_ll!(bb.b°, mcmciter)
+end
+
+"""
+    ll_of_accepted(bb::BiBlock, i)
+
+Return the log-likelihood of the path that was accepted at the `i`th iteration.
+"""
+function ll_of_accepted(bb::BiBlock, i)
+    return (bb.accpt_history[i] ? bb.b°.ll_history[i] : bb.b.ll_history[i])
+end
+
+"""
+    GP.set_obs!(bb::BiBlock)
+
+Freeze an artificial observation at the terminal point of the block. For a
+terminal block nothing is done.
+"""
+function GP.set_obs!(bb::BiBlock) end
+
+function GP.set_obs!(bb::BiBlock{true})
+    set_obs!(bb.b.P_last, bb.b.XX[end].x[end])
+    set_obs!(bb.b°.P_last, bb.b.XX[end].x[end])
+end
+
+GP.set_obs!(bb::BiBlock{false}) = nothing
+
+
+"""
+    GP.recompute_guiding_term!(bb::BiBlock)
+
+Recompute the guiding terms of both the proposal and the accepted laws.
+"""
+function GP.recompute_guiding_term!(bb::BiBlock)
+    recompute_guiding_term!(bb.b)
+    recompute_guiding_term!(bb.b°)
+end
+
+"""
+    find_W_for_X!(bb::BiBlock)
+
+Find the Wiener process `bb.b.WW` that reconstructs path `bb.b.XX` under the
+accepted law `bb.b.PP` (possibly including `bb.b.P_last`).
+"""
+find_W_for_X!(bb::BiBlock) = find_W_for_X!(bb.b)
